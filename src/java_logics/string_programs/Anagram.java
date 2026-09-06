fa1c6029cc7da0e3f39712750ff7ef45b6ee4a21
Pradeep Aili <ailipradeep5@gmail.com>
2026-09-06 20:28:21 +0530

    added a anagram program

---
+++ src/java_logics/string_programs/Anagram.java
@@ -0,0 +1,29 @@
+package java_logics.string_programs;
+
+import java.util.Arrays;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		String s1 = "listen";
+		String s2 = "silent";
+		
+		if(s1.length() == s2.length())
+		{
+			char[] ch1=s1.toCharArray();
+			char[] ch2=s2.toCharArray();
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+			if(Arrays.equals(ch1, ch2))
+			{
+				System.out.println("Anagram");
+			}
+			else {
+				System.out.println("Not a Anagram");
+			}			
+		}
+		else {
+			System.out.println("Not a Anagram");
+		}
+	}
+}
